0172b577768cf95feed62d01073e0605f1017004
SamiNousiainen <n1nosa00@students.oamk.fi>
2022-03-15 12:50:18 +0200

    kuva testi

--- src/App.js
+++ src/App.js
@@ -7,6 +7,10 @@ function App() {
     <div id='container'>
       <header>Hiiret & hiirimatot OY</header>
       <div>Tänne niitä hiiriä ja hiirimattoja kiitos</div>
+      <div>
+      <img src="../hiiri.jpg" alt="" />
+      <p>tässä ois 1</p>
+      </div>
       <footer>&copy; Tiimi 10</footer>
     </div>
   );
