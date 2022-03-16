f90fb3689745d35d4d1b7a5af63e4bc4c13cfebe
SamiNousiainen <n1nosa00@students.oamk.fi>
2022-03-16 10:54:29 +0200

    sisennykset

--- src/App.js
+++ src/App.js
@@ -14,17 +14,17 @@ function App() {
       <img src="../hiiri.jpg" alt="" />
       <p>tässä ois 1</p>
 
-      <Card style={{ width: '18rem' }}>
-  <Card.Img variant="top" src="holder.js/100px180" />
-  <Card.Body>
-    <Card.Title>Card Title</Card.Title>
-    <Card.Text>
-      Some quick example text to build on the card title and make up the bulk of
-      the card's content.
-    </Card.Text>
-    <Button variant="primary">Go somewhere</Button>
-  </Card.Body>
-</Card>
+        <Card style={{ width: '18rem' }}>
+          <Card.Img variant="top" src="holder.js/100px180" />
+          <Card.Body>
+            <Card.Title>Card Title</Card.Title>
+            <Card.Text>
+              Some quick example text to build on the card title and make up the bulk of
+              the card's content.
+            </Card.Text>
+            <Button variant="primary">Go somewhere</Button>
+          </Card.Body>
+        </Card>
       
       </div>
       <footer>&copy; Tiimi 10</footer>
