6208024bbf60d49d5b388e60cf3352c2a27b75ac
WhiteZett <niklas.haro@gmail.com>
2022-03-16 14:18:18 +0200

    Lisäsin 'hiiret', sekä 'hiirimatot' sivut

---
+++ src/Hiiret.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Hiiret() {
+    return (
+        <div>
+            <h1>Hiiret</h1>
+        </div>
+    );
+}
+
+export default Hiiret;
---
+++ src/Hiirimatot.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Hiirimatot() {
+    return (
+        <div>
+            <h1>Hiirimatot</h1>
+        </div>
+    );
+}
+
+export default Hiirimatot;
