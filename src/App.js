1b035bbdc2fb28359ad5f2cb2c0067d62bccd90d
Toni Leinonen <tonimatias96@gmail.com>
2022-03-16 13:22:37 +0200

    Navbar

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
   return (
     <div id=''>
       {/* <header>Timon pelivärkit </header> */}
-      <Navbar bg="dark" expand="lg">
+    {/*   <Navbar bg="dark" expand="lg">
         <Container fluid>
           <Navbar.Brand href="<App />"> <img src='../Gaming Gadgets.png' className='logo' alt='logo'></img></Navbar.Brand>
           <Navbar.Toggle aria-controls="navbarScroll" />
@@ -47,7 +47,42 @@ function App() {
             </Form>
           </Navbar.Collapse>
         </Container>
-      </Navbar>
+      </Navbar> */}
+
+<Navbar bg="dark" expand="lg">
+  <Container fluid>
+  <Navbar.Brand href="<App />"> <img src='../Gaming Gadgets.png' className='logo' alt='logo'></img></Navbar.Brand>
+    <Navbar.Toggle aria-controls="navbarScroll" />
+    <Navbar.Collapse id="navbarScroll">
+      <Nav
+        className="me-auto my-2 my-lg-0"
+        style={{ maxHeight: '100px' }}
+        navbarScroll
+      >
+        
+        <Nav.Link href="#action2" id='link'>Link1</Nav.Link>
+        <Nav.Link href="#" id='link'>Link2</Nav.Link>
+        <NavDropdown title="Categories" id="navbarScrollingDropdown">
+          <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
+          <NavDropdown.Item href="#action4">Another action</NavDropdown.Item>
+          <NavDropdown.Divider />
+          <NavDropdown.Item href="#action5">
+            Something else here
+          </NavDropdown.Item>
+        </NavDropdown>
+      </Nav>
+      <Form className="d-flex">
+        <FormControl
+          type="search"
+          placeholder="Search"
+          className="me-2"
+          aria-label="Search"
+        />
+        <Button variant="outline-success">Search</Button>
+      </Form>
+    </Navbar.Collapse>
+  </Container>
+</Navbar>
       <div>Tänne niitä hiiriä ja hiirimattoja kiitos</div>
       <div>
         <p>tässä ois 1</p>
