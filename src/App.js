ed852b0572997269fcbefa1aee775960a122ba75
SamiNousiainen <n1nosa00@students.oamk.fi>
2022-03-23 10:27:59 +0200

    footer muutos

--- src/App.js
+++ src/App.js
@@ -54,13 +54,13 @@ function App() {
       <Carousel id='carousel' variant='dark'>
   <Carousel.Item>
     <img
-      className="d-block w-50"
+      className="d-block w-100"
       src="../hiiri.jpg"
       alt="First slide"
     />
     <Carousel.Caption>
       <h3>Hiiri 1</h3>
-      <p>iha saatanan hyvä hiiri</p>
+      <p>iha saatanan leveä hiiri</p>
       <div id='hinta'><span id='vanha-hinta'>5,95</span> 4,95</div>
     </Carousel.Caption>
   </Carousel.Item>
