783848db82284cbe6d7e3215b4fe2484d6ffa964
NiklasHr <niklas.haro@gmail.com>
2022-03-24 12:53:36 +0200

    footer lisätty components kansioon

---
+++ src/components/Footer.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+function Footer() {
+    return (
+        <div>
+            <footer>
+                <div>
+                    &copy; Ryhmä 10
+                </div>
+
+                <div>
+                    dfsfdgfg
+                </div>
+            </footer>
+        </div>
+    );
+}
+
+export default Footer;
