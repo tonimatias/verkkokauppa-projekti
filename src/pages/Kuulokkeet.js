59f5eeca76e7c7adb62c084050db2ccef0a7028a
tonimatias <tonimatias96@gmail.com>
2022-03-24 13:52:48 +0200

    kuulokkeet js sivun alottelua

--- src/pages/Kuulokkeet.js
+++ src/pages/Kuulokkeet.js
@@ -2,10 +2,13 @@ import React from "react";
 
 
 function Kuulokkeet() {
+
+    
+
     return (
         <div>
             <h1>Kuulokkeet</h1>
-            {products.map(product => (
+           {/* {products.map(product => (
 
             
             <div class="card" style="width: 18rem;" key={product.id}>
@@ -13,11 +16,11 @@ function Kuulokkeet() {
                     <div class="card-body">
                         <h5 class="card-title">{product.name}</h5>
                         <p class="card-text">{product.description} <br></br> {product.price}</p>
-                        {/* <a href="#" class="btn btn-primary">Go somewhere</a> */}
+                        
                         <button variant="primary"><img className='shoppingcartIcon' src='../cart.png' title='Lisää ostoskoriin' alt='hiiri2'></img></button>
                     </div>
             </div>
-            ))}
+            ))} */}
         </div>
     );
 }
