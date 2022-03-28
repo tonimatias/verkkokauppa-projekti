398842f8d2b296b293ab7c8f53c22da5bf905b9a
WhiteZett <niklas.haro@gmail.com>
2022-03-28 12:22:47 +0300

    logo laitettiin viemään etusivulle

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ export default function Header() {
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-dark">
   <div className="container-fluid">
-    <a className="logo" href="#"><img id='logo' src='../Gaming Gadgets.png'></img></a>
+    <Link className="logo" to="/"><img id='logo' src='../Gaming Gadgets.png'></img></Link>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
