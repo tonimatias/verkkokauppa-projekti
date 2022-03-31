665013acd2cbce7ea8050eab7ee6b7e4a56b327d
NiklasHr <niklas.haro@gmail.com>
2022-03-31 14:11:44 +0300

    footeriin parannetut yhteystiedot

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,11 +5,13 @@ export default function Footer() {
         <div class="footer-container">
             <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"></link>
             <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 border-top">
-                <div class="col-md-4 d-flex align-items-center">
-                    <a href="/" class="mb-3 me-2 mb-md-0 text-muted text-decoration-none lh-1">
-                        <i class="bi bi-twitter"></i>
-                    </a>
-                    <span>&copy; Gaming Gadgets, +358 666 66666</span>
+                <div class="col-md-4">
+                    <span>&copy; Gaming Gadgets</span>
+                </div>
+                <div class="col-md-4">
+                    <span>Yhteystiedot: </span><br />
+                    <span>Puhelin: +666 666 66666</span><br />
+                    <span>Sähköposti: gaming.gadgets@giimeil.com</span>
                 </div>
 
                 <ul class="nav col-md-4 justify-content-end list-unstyled d-flex">
