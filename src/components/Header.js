eed6f5a916105ba144e2335b625752da92237b4e
SamiNousiainen <n1nosa00@students.oamk.fi>
2022-04-07 15:14:50 +0300

    hakukenttä poistettu

--- src/components/Header.js
+++ src/components/Header.js
@@ -53,10 +53,6 @@ export default function Header({url,cart}) {
         </li>
       </ul>
 
-      <form className="d-flex">
-        <input className="form-control me-2" type="search" placeholder="Kirjoita hakusana" aria-label="Search"/>
-       {/*  <button className="btn btn-outline-success" type="submit">Hae</button> */}
-      </form>
       <ul className='navba-nav ml-auto' id='shoppingCart'>
         <li className='nav-item'>
             <Cart cart={cart}/>
