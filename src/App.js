211a8b21f60e568196ae17a2512225ce69a2a305
tonimatias <tonimatias96@gmail.com>
2022-04-07 14:37:07 +0300

    ostoskorin tuotelisäys (ei toimi vielä)

--- src/App.js
+++ src/App.js
@@ -21,16 +21,40 @@ function App() {
   }, [])
   
 
-  function addToCart(product) {
+/*   function addToCart(product) {
     const newCart = [...cart,product];
     setCart(newCart);
     localStorage.setItem('cart',JSON.stringify(newCart));
   }
-
+ */
   function removeFromCart(product) {
     const itemsWithoutRemoved = cart.filter(item => item.id !== product.id);
     setCart(itemsWithoutRemoved);
     localStorage.setItem('cart',JSON.stringify(itemsWithoutRemoved));
+  }
+
+  function updateAmount(amount, product){
+    product.amout = amount;
+    const index = cart.findIndex((item => item.id === product.id));
+    const modifiedCart = Object.assign([...cart],{[index]: product});
+    setCart(modifiedCart);
+    localStorage.setItem('cart',JSON.stringify(modifiedCart));
+  }
+
+  function addToCart(product){
+    if (cart.some(item => item.id === product.id)) {
+      const existingProduct = cart.filter(item => item.id === product.id);
+      updateAmount(parseInt(existingProduct[0].amount) + 1,product);
+    } else {
+      product['amount'] = 1;
+      const newCart = [...cart,product];
+      setCart(newCart);
+      localStorage.setItem('cart',JSON.stringify(newCart));
+    }
+      /*  product['amount'] = 1;
+      const newCart = [...cart,product];
+      setCart(newCart);
+      localStorage.setItem('cart',JSON.stringify(newCart)); */
   }
 
   return (
--- src/pages/Order.js
+++ src/pages/Order.js
@@ -1,26 +1,52 @@
 import React from 'react';
 import uuid from 'react-uuid';
+import {useState, useEffect} from 'react';
 
-export default function Order({cart, removeFromCart}) {
+export default function Order({cart,removeFromCart,updateAmount}) {
+    const [inputs,_] = useState([]);
+    const [inputIndex, setInputIndex] = useState(-1);
+
     let sum = 0;
+
+    function changeAmount(e,product,index) {
+        updateAmount(e.target.value,product);
+        setInputIndex(index);
+    }
+
+    useEffect(() => {
+      for (let i = 0;i<cart.length;i++){
+          inputs[i] = React.createRef();
+      }
+    }, [cart.length])
+    
+    useEffect(()=> {
+        if (inputs.length > 0 && inputIndex > -1 && inputs[inputIndex].current !== null) {
+            inputs[inputIndex].current.focus();
+        }
+    },[cart])
+
   return (
     <div>
         <h3 className='header'>Ostoskorin tuotteet</h3>
         <table className='table'>
             <tbody>
-                {cart.map(product => {
+                {cart.map((product,index) => {
                     sum+=parseFloat(product.price);
                     return (
                         <tr key={uuid()}>
                             <td>{product.name}</td>
                             <td>{product.price} €</td>
+                            <td>
+                                <input /* type='number' */ ref={inputs[index]} style={{width: '60px'}} value={product.amount} onChange={e => changeAmount(e,product)}></input>
+                            </td>
                             <td><a href='#' onClick={() => removeFromCart(product)}>Poista</a></td>
                         </tr>
                     )
                 })}
                 <tr key={uuid()}>
-                    <td></td>
+                    <td>Yhteensä: </td>
                     <td>{sum.toFixed(2)} €</td>
+                    <td></td>
                     <td></td>
                 </tr>
             </tbody>
