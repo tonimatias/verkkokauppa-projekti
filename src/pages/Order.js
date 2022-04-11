abefc36162e0b784f55407a4e5b1d1a72f4d2cc9
tonimatias <tonimatias96@gmail.com>
2022-04-11 12:23:45 +0300

    ostosten summan näyttäminen

--- src/pages/Order.js
+++ src/pages/Order.js
@@ -31,11 +31,11 @@ export default function Order({cart,removeFromCart,updateAmount}) {
         <table className='table'>
             <tbody>
                 {cart.map((product,index) => {
-                    sum+=parseFloat(product.price);
+                    sum+=parseFloat(product.price) * product.amount;
                     return (
                         <tr key={uuid()}>
                             <td>{product.name}</td>
-                            <td>{product.price} €</td>
+                            <td>{product.price.toFixed(2)} €</td>
                             <td>
                                 <input /* type='number' */ ref={inputs[index]} style={{width: '60px'}} value={product.amount} onChange={e => changeAmount(e,product)}></input>
                             </td>
