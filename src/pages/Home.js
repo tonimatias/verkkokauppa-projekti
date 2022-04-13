a6715759dc71964ab5cfdaf523ca7ddde3687fec
SamiNousiainen <n1nosa00@students.oamk.fi>
2022-04-13 10:53:13 +0300

    etusivu korjattu

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -7,31 +7,33 @@ import { Link, useParams } from "react-router-dom"
 
 export default function Home({url,addToCart}) {
 
-       const [products, setProducts] = useState([]);
+        const [products, setProducts] = useState([]);
     
-        let params = useParams();
+         let params = useParams();
     
-        useEffect(() => {
-            axios.get(url + 'products/getdiscountproducts.php/')
-            .then((response) => {
+         useEffect(() => {
+             axios.get(url + 'products/getdiscountproducts.php/')
+             .then((response) => {
                 const json = response.data;
-                setProducts(json.products)
-            }).catch(error => {
-                alert(error.response === undefined ? error : error.response.data.error);
-            })
+                 setProducts(json.products)
+                }).catch(error => {
+             alert(error.response === undefined ? error : error.response.data.error);
+             })
         }, [params])
 
-    // const [popularProducts, setPopularProducts] = useState([]);
-
-    //     useEffect(() => {
-    //         axios.get(url + 'products/getpopularproducts.php/')
-    //             .then((response) => {
-    //                 const json = response.data;
-    //                 setPopularProducts(json.popularProducts)
-    //             }).catch(error => {
-    //                 alert(error.response === undefined ? error : error.response.data.error);
-    //             })
-    //     }, [params])
+         const [popularProducts, setPopularProducts] = useState([]);
+    
+   
+        useEffect(() => {
+             axios.get(url + 'products/getpopularproducts.php/')
+             .then((response) => {
+                const json = response.data;
+                 setPopularProducts(json.products)
+                }).catch(error => {
+             alert(error.response === undefined ? error : error.response.data.error);
+             })
+        }, [params])
+ 
 
     return (
         <section id='content'>
@@ -40,11 +42,11 @@ export default function Home({url,addToCart}) {
             <div id="discountProducts">
             {products.map(product => (
                 <div key={product.id}>
-                    <div class="card" style={{width: 20 + 'rem', height: 24 + 'rem'}}>
-                        <img src={url + 'images/' + product.image} class="card-img-top" alt="img"/>
-                            <div class="card-body">
-                                <h5 class="card-title">{product.name}</h5>
-                                <p class="card-text">{product.description}</p>
+                    <div className="card" style={{width: 20 + 'rem', height: 24 + 'rem'}}>
+                        <img src={url + 'images/' + product.image} className="card-img-top" alt="img"/>
+                            <div className="card-body">
+                                <h5 className="card-title">{product.name}</h5>
+                                <p className="card-text">{product.description}</p>
                                 <div id="cardBottom">
                                 <span id='hinta'>{product.price.toFixed(2)}</span>
                                 <span id='vanhaHinta'>{product.old_price}</span>
@@ -54,22 +56,22 @@ export default function Home({url,addToCart}) {
                     </div>
                 </div>
             ))}
-            </div> 
+            </div>
 
             <h3>Suosituimmat</h3>
 
-             <div id="discountProducts">
-            {products.map(popular_product => (
-                <div key={popular_product.id}>
-                    <div class="card" style={{width: 20 + 'rem', height: 24 + 'rem'}}>
-                        <img src={url + 'images/' + popular_product.image} class="card-img-top" alt="img"/>
-                            <div class="card-body">
-                                <h5 class="card-title">{popular_product.name}</h5>
-                                <p class="card-text">{popular_product.description}</p>
+            <div id="discountProducts">
+            {popularProducts.map(product => (
+                <div key={product.id}>
+                    <div className="card" style={{width: 20 + 'rem', height: 24 + 'rem'}}>
+                        <img src={url + 'images/' + product.image} className="card-img-top" alt="img"/>
+                            <div className="card-body">
+                                <h5 className="card-title">{product.name}</h5>
+                                <p className="card-text">{product.description}</p>
                                 <div id="cardBottom">
-                                <span id='hinta'>{popular_product.price.toFixed(2)}</span>
-                                <span id='vanhaHinta'>{popular_product.old_price}</span>
-                                <Button id="addToCart" onClick={e => addToCart(popular_product)} variant="primary"><img className='shoppingcartIcon' src='../cart.png' title='Lisää ostoskoriin' alt='productImg'/></Button>
+                                <span id='hinta'>{product.price.toFixed(2)}</span>
+                                <span id='vanhaHinta'>{product.old_price}</span>
+                                <Button id="addToCart" onClick={e => addToCart(product)} variant="primary"><img className='shoppingcartIcon' src='../cart.png' title='Lisää ostoskoriin' alt='productImg'/></Button>
                                 </div>
                             </div>
                     </div>
