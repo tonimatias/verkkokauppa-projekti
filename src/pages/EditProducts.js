cb25cd5cd902ab3435d7e3596d0f438424df2a8f
NiklasHr <niklas.haro@gmail.com>
2022-04-27 15:32:05 +0300

    kuvauksen kirjoittamiselle lisää tilaa

--- src/pages/EditProducts.js
+++ src/pages/EditProducts.js
@@ -99,12 +99,12 @@ export default function ManageProducts({url}) {
                         <input type="text" value={oldPrice} onChange={(e) => setOldPrice(e.target.value)}/>
                     </div>
                     <div>
-                    <label>Tuotteen ID</label>
-                        <input type="text" value={productId} onChange={(e) => setProductId(e.target.value)}/>
+                        <label>Kuvaus</label>
+                        <textarea id='desc' type="text" rows="4" cols="40"value={productDescription} onChange={(e) => setProductDescription(e.target.value)}/>
                     </div>
                     <div>
-                        <label>Kuvaus</label>
-                        <input id='desc' type="text" value={productDescription} onChange={(e) => setProductDescription(e.target.value)}/>
+                    <label>Tuotteen ID</label>
+                        <input type="text" value={productId} onChange={(e) => setProductId(e.target.value)}/>
                     </div>
                     <button className="btn btn-dark" type="submit">Tallenna</button>
                 </form>
